6472ff6d7d9c837dc298282989024bfd029aaab5
Alicia Medina Sillero <1709497@uab.cat>
2025-10-08 10:17:01 +0200

    class changes

--- js/funcions.js
+++ js/funcions.js
@@ -2,7 +2,7 @@ function confirmaRegistre(){
     //completa
     alert("Registrant estudiant...");
     console.log("Registrant estudiant...");
-    const div = document.getElementById("formDiv");
+    let div = document.getElementById("formDiv");
     div.innerHTML = "<p class='important'>Formulari enviat correctament!</p>";
     return false;
 }
